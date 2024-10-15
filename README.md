d36b5033d5a7036f3339b4b20d7546a25c626e25
Helio Frota <00hf11@gmail.com>
2024-10-14 23:05:40 -0300

    hello world bevy

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # rgold
 
+## Dependencies
+
+https://github.com/bevyengine/bevy/blob/latest/docs/linux_dependencies.md#fedora
+
+and had to install this to build
+
+```
+sudo dnf install 'pkgconfig(libudev)'
+```
+
 ## Run
 
 ```console
 cargo run
 ```
 
-* type 'h'
-* type 'q' or 'ESC' to quit
-
-Current status
-
-
-![current status](status.png)
--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,105 @@
-// ----------
-// Module imports necessary to run the app.
-// https://ratatui.rs/tutorials/hello-world/#imports
-use crossterm::{
-    event::{self, Event::Key, KeyCode},
-    terminal::{
-        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
-        LeaveAlternateScreen,
-    },
-    ExecutableCommand,
-};
+// // ----------
+// // Module imports necessary to run the app.
+// // https://ratatui.rs/tutorials/hello-world/#imports
+// use crossterm::{
+//     event::{self, Event::Key, KeyCode},
+//     terminal::{
+//         disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+//         LeaveAlternateScreen,
+//     },
+//     ExecutableCommand,
+// };
 
-use ratatui::{
-    prelude::{CrosstermBackend, Terminal},
-    style::Color,
-    symbols::Marker,
-    widgets::canvas::*,
-};
+// use ratatui::{
+//     prelude::{CrosstermBackend, Terminal},
+//     style::Color,
+//     symbols::Marker,
+//     widgets::canvas::*,
+// };
 
-// ----------
+// // ----------
 
-use std::{
-    io::{self, stdout, Stdout},
-    time::Duration,
-};
+// use std::{
+//     io::{self, stdout, Stdout},
+//     time::Duration,
+// };
 
-mod player;
+// mod player;
 
-use player::Player;
+// use player::Player;
 
-// ----------
-// Required `init` and `restore` functions.
-// https://ratatui.rs/tutorials/hello-world/#setting-up-and-restoring-the-terminal
-fn init_terminal() -> io::Result<Terminal<CrosstermBackend<Stdout>>> {
-    enable_raw_mode()?;
-    stdout().execute(EnterAlternateScreen)?;
-    Terminal::new(CrosstermBackend::new(stdout()))
+// // ----------
+// // Required `init` and `restore` functions.
+// // https://ratatui.rs/tutorials/hello-world/#setting-up-and-restoring-the-terminal
+// fn init_terminal() -> io::Result<Terminal<CrosstermBackend<Stdout>>> {
+//     enable_raw_mode()?;
+//     stdout().execute(EnterAlternateScreen)?;
+//     Terminal::new(CrosstermBackend::new(stdout()))
+// }
+
+// fn restore_terminal() -> io::Result<()> {
+//     disable_raw_mode()?;
+//     stdout().execute(LeaveAlternateScreen)?;
+//     Ok(())
+// }
+// // ----------
+
+// fn main() -> io::Result<()> {
+//     let mut terminal = init_terminal()?;
+
+//     let count: i8 = 60;
+//     let mut p = Player::new(0.0, 0.0);
+
+//     // ----------
+//     // the game loop
+//     loop {
+//         terminal.draw(|frame| {
+//             let area = frame.area();
+
+//             let the_canvas = Canvas::default()
+//                 .marker(Marker::Bar)
+//                 .x_bounds([-180.0, 180.0])
+//                 .y_bounds([-90.0, 90.0])
+//                 .paint(|ctx| {
+//                     ctx.draw(&Line {
+//                         x1: -150.0,
+//                         y1: 75.0,
+//                         x2: 150.0,
+//                         y2: 75.0,
+//                         color: Color::Cyan,
+//                     });
+
+//                     ctx.layer();
+
+//                     ctx.print(-150.0, 80.0, format!("SCORE: {}", p.score));
+//                     ctx.print(-30.0, 80.0, format!("STEPS: {}", p.steps));
+//                     ctx.print(75.0, 80.0, format!("TIME REMAINING: {}", count));
+//                 });
+
+//             frame.render_widget(the_canvas, area);
+//         })?;
+
+//         if event::poll(Duration::from_millis(250))? {
+//             if let Key(ke) = event::read()? {
+//                 if ke.code == KeyCode::Char('q') || ke.code == KeyCode::Esc {
+//                     break;
+//                 } else if ke.code == KeyCode::Char('h') {
+//                     p.steps += 1;
+//                 }
+//             }
+//         }
+//     }
+//     // ----------
+
+//     restore_terminal()
+// }
+
+use bevy::prelude::*;
+
+fn hello_world() {
+    println!("hello world!");
 }
 
-fn restore_terminal() -> io::Result<()> {
-    disable_raw_mode()?;
-    stdout().execute(LeaveAlternateScreen)?;
-    Ok(())
-}
-// ----------
-
-fn main() -> io::Result<()> {
-    let mut terminal = init_terminal()?;
-
-    let count: i8 = 60;
-    let mut p = Player::new(0.0, 0.0);
-
-    // ----------
-    // the game loop
-    loop {
-        terminal.draw(|frame| {
-            let area = frame.area();
-
-            let the_canvas = Canvas::default()
-                .marker(Marker::Bar)
-                .x_bounds([-180.0, 180.0])
-                .y_bounds([-90.0, 90.0])
-                .paint(|ctx| {
-                    ctx.draw(&Line {
-                        x1: -150.0,
-                        y1: 75.0,
-                        x2: 150.0,
-                        y2: 75.0,
-                        color: Color::Cyan,
-                    });
-
-                    ctx.layer();
-
-                    ctx.print(-150.0, 80.0, format!("SCORE: {}", p.score));
-                    ctx.print(-30.0, 80.0, format!("STEPS: {}", p.steps));
-                    ctx.print(75.0, 80.0, format!("TIME REMAINING: {}", count));
-                });
-
-            frame.render_widget(the_canvas, area);
-        })?;
-
-        if event::poll(Duration::from_millis(250))? {
-            if let Key(ke) = event::read()? {
-                if ke.code == KeyCode::Char('q') || ke.code == KeyCode::Esc {
-                    break;
-                } else if ke.code == KeyCode::Char('h') {
-                    p.steps += 1;
-                }
-            }
-        }
-    }
-    // ----------
-
-    restore_terminal()
+fn main() {
+    App::new().add_systems(Update, hello_world).run();
 }
